d6c0c9f5c8a1ca13d6bd1a2164c0b0711b101012
타래 <tarae7617@naver.com>
2023-02-24 16:17:16 +0900

    [unrated] Title: 숨어있는 숫자의 덧셈 (2), Time: 0.16 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.js
+++ 프로그래머스/unrated/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.js
@@ -1,8 +1,3 @@
 function solution(my_string) {
-    let arr = my_string.match(/\d+/g) ?? 0;
-    let result = 0;
-    for (let i = 0; i < arr.length; i++) {
-        result += parseInt(arr[i]);
-    }
-    return result
+    return my_string.split(/\D+/).reduce((acc, cur) => acc + Number(cur), 0);
 }
