c32735fd9d3c96fb83f06e7d6f70535b1fcbe08b
타래 <tarae7617@naver.com>
2023-04-17 22:57:28 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
+++ 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
@@ -1,5 +1,5 @@
 function solution(numbers) {
     let result = 45;
-    numbers.sort((a, b) => a - b).forEach((v) => (result -= v));
+    numbers.forEach((v) => (result -= v));
     return result;
 }
