c3bc5eafac057e381f9632d9bed005334cb2ce80
타래 <tarae7617@naver.com>
2023-04-23 15:37:18 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 44.04 ms, Memory: 82.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
+++ 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -1,4 +1,10 @@
 function solution(arr)
 {
+    // return (arr.filter((v, i) => v !== arr[i - 1])); 안됨
+    // arr 배열을 필터링하는 동안, arr 배열 자체가 변경되기때문에 시간이 오래걸림
+    // !==는 타입과 값 둘 다 신경쓰기 때문에 시간이 더 걸리는듯..?
+    
+    // return (arr.filter((v, i) => v != arr[i - 1])); 됨
+    // return (arr.filter((v, i, array) => v !== array[i - 1])); 됨
     return (arr.filter((v, i) => v != arr[i - 1]));
 }
