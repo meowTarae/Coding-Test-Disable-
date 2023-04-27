77c91067ac34af54751a6125073c0018b206bc27
타래 <tarae7617@naver.com>
2023-04-27 22:37:19 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.15 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
+++ 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
@@ -1,11 +1,12 @@
 function solution(s) {
-const newArr = [];
-let cnt = 0;
-for (const i of s.toLowerCase()) {
-  cnt = i === " " ? 0 : (cnt += 1);
-  const word = cnt % 2 === 0 ? i : i.toUpperCase();
-  newArr.push(word);
-}
-
-return (newArr.join(""));
+    const newArr = [];
+    let cnt = 0;
+    
+    for (const i of s.toLowerCase()) {
+      cnt = i === " " ? 0 : (cnt += 1);
+      const word = cnt % 2 === 0 ? i : i.toUpperCase();
+      newArr.push(word);
+    }
+    
+    return (newArr.join(""));
 }
