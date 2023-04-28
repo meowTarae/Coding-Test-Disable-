aea2d942d359a45fd327ad9b7294690acd9747fe
타래 <tarae7617@naver.com>
2023-04-28 09:00:45 +0900

    [level 1] Title: 최소직사각형, Time: 872.45 ms, Memory: 80.5 MB -BaekjoonHub

--- 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.js
+++ 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.js
@@ -1,10 +1,20 @@
 function solution(sizes) {
-    const w = sizes.map((v) => v.sort((a, b) => a - b)).sort((a, b) => a[0] - b[0])[
+// 1
+const w = sizes.map((v) => v.sort((a, b) => a - b)).sort((a, b) => a[0] - b[0])[
   sizes.length - 1
 ][0];
-    const h = sizes.reduce((acc, cur) => [...acc, ...cur]).sort((a, b) => a - b)[
+const h = sizes.reduce((acc, cur) => [...acc, ...cur]).sort((a, b) => a - b)[
   sizes.length * 2 - 1
 ];
+return w*h
+    
+// 2
+const newArr = sizes.map(([w, h]) => w < h ? [h, w] : [w, h]);
 
-    return (w*h);
+let max = [0, 0];
+newArr.forEach(([w, h]) => {
+    if (w > max[0]) max[0] = w;
+    if (h > max[1]) max[1] = h;
+})
+return max[0]*max[1];
 }
