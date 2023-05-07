8bb97dcf531bf32719b3619d702bb90f7b25ddf7
타래 <tarae7617@naver.com>
2023-05-07 17:09:42 +0900

    [unrated] Title: 가장 가까운 같은 글자, Time: 2.13 ms, Memory: 36.8 MB -BaekjoonHub

--- 프로그래머스/unrated/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
+++ 프로그래머스/unrated/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
@@ -1,13 +1,20 @@
 function solution(s) {
-return (
-  [...s]
-    .reverse()
-    .map((v, i, arr) => {
-      for (let j = i + 1; j < arr.length; j++) {
-        if (v === arr[j]) return ~~(j - i);
-      }
-      return -1;
-    })
-    .reverse()
-);
+// 1번
+// console.log(
+//   [...s]
+//     .reverse()
+//     .map((v, i, arr) => {
+//       for (let j = i + 1; j < arr.length; j++) {
+//         if (v === arr[j]) return ~~(j - i);
+//       }
+//       return -1;
+//     })
+//     .reverse()
+// );
+
+// 2번
+return [...s].map((v, i) => {
+  const cnt = s.substring(0, i).lastIndexOf(v);
+  return cnt < 0 ? cnt : i - cnt;
+});
 }
