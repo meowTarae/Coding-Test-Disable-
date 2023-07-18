9d9383c70f4fc2cb65b8c04c40de57f250899913
타래 <tarae7617@naver.com>
2023-07-18 16:17:18 +0900

    [unrated] Title: 귤 고르기, Time: 1044.88 ms, Memory: 56.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/138476. 귤 고르기/귤 고르기.js
@@ -0,0 +1,15 @@
+function solution(k, tangerine) {
+    const storage = [0];
+    tangerine.forEach(v=>storage[v] ? storage[v]++ : storage[v]=1);
+    
+    storage.sort((a,b)=> b-a);
+    
+    let count = 0;
+    for(let i=0; i<storage.length; i++){
+        count++;
+        if(storage[i]>=k) break;
+        else k-= storage[i];    
+    }
+    
+    return count;
+}
