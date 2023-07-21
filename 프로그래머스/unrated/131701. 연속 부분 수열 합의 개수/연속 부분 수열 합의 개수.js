32499c22d400b005366f013c0758f5f93d77fcc2
타래 <tarae7617@naver.com>
2023-07-21 16:55:54 +0900

    [unrated] Title: 연속 부분 수열 합의 개수, Time: 1099.81 ms, Memory: 60.2 MB -BaekjoonHub

--- 프로그래머스/unrated/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.js
+++ 프로그래머스/unrated/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.js
@@ -2,10 +2,7 @@ function solution(elements) {
     const set = new Set();
     const newArr = [...elements, ...elements];
     
-    for(let i=1; i<=elements.length; i++){
-        if(i===1) set.add(...elements); 
-        if(i===elements.length) set.add(elements.reduce((a,b)=>a+b));
-        
+    for(let i=1; i<=elements.length; i++){      
         for(let j=0; j<elements.length; j++){
             let sum=0;
             for(let k=0; k<i; k++)
