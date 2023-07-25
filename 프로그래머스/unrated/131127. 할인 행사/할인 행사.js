f7d8f3dba90bc904f7c220ffd78701cdf4145497
타래 <tarae7617@naver.com>
2023-07-25 14:50:56 +0900

    [unrated] Title: 할인 행사, Time: 2.32 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/131127. 할인 행사/할인 행사.js
@@ -0,0 +1,30 @@
+// 1412
+function solution(want, number, discount) {
+    let count = 0;
+    const numberCount = number.reduce((a,b)=>a+b);
+    const wantObj = {};
+    for(let i=0; i<want.length; i++){
+        wantObj[want[i]] = number[i];
+    }
+    const wantObjToArray = Object.entries(wantObj).sort().toString();
+    console.log(wantObjToArray)
+    
+    const makeObj = (arr) =>{
+        const obj = {};
+        if (arr.length !== numberCount) return obj;
+        
+        for(let i=0; i<numberCount; i++){
+            obj[arr[i]] = (obj[arr[i]] || 0) + 1;
+        }
+        return obj;
+    }
+    
+    for(let i=0; i<discount.length; i++){
+        const compareObj = makeObj(discount.slice(i,i+numberCount));
+        if(Object.keys(compareObj).length !== 0)
+            if(wantObjToArray == Object.entries(compareObj).sort().toString())
+                count++;
+    }
+    
+    return count;
+}
